0c9e8ca050e0bcd0ce4b0fabdb9f574427d7e2e5
EduardMikhrin <emihrin50@gmail.com>
2025-05-26 13:53:14 +0300

    fix sql migrations

--- database/schema/17-gov-proposals.sql
+++ database/schema/17-gov-proposals.sql
@@ -4,3 +4,6 @@ ALTER COLUMN content TYPE TEXT USING content::text,
     ADD COLUMN metadata TEXT;
 
 -- +migrate Down
+ALTER TABLE proposal
+ALTER COLUMN content TYPE JSONB USING content::jsonb,
+    DROP COLUMN metadata;

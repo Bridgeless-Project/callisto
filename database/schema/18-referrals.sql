adcce45051ffd73fcc4b45a45a66e813092ace92
MarkCherepovskyi <mark.cherepovskyi@gmail.com>
2025-10-29 11:52:57 +0200

    update migrations

--- database/schema/18-referrals.sql
+++ database/schema/18-referrals.sql
@@ -1,21 +1,29 @@
 -- +migrate Up
-CREATE TABLE referral {
-    id INT PRIMARY KEY,
+CREATE DOMAIN uint16 AS integer
+    CHECK (VALUE BETWEEN 0 AND 65535);
+
+
+CREATE TABLE referral
+(
+    id uint16 PRIMARY KEY,
     withdrawal_address TEXT,
     commission_rate INT
-};
+);
 
-CREATE TABLE referral_rewards {
-    referral_id UINT,
-    token_id BIGINT,
-    to_claim string,
-    total_claimed_amount string,
-};
+CREATE TABLE referral_rewards
+(
+    referral_id          uint16,
+    token_id             BIGINT,
+    to_claim             string,
+    total_claimed_amount string
+);
 
-ALTER TABLE bridge_transactions ADD COLUMN referral_id INT DEFAULT 0;
+ALTER TABLE bridge_transactions ADD COLUMN uint16 INT DEFAULT 0;
 
 -- +migrate Down
 DROP TABLE referral;
 DROP TABLE referral_rewards;
 
 ALTER TABLE bridge_transactions DROP COLUMN referral_id;
+
+DROP DOMAIN uint16;

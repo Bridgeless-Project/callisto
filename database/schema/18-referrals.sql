74982ff08b62f4aa4294ab3b74b276fa0d0b271f
Eduard Mikhrin <48176280+EduardMikhrin@users.noreply.github.com>
2025-10-29 16:41:16 +0200

    Fix/migrations-18 (#13)
    * update migrations
    
    * fix migrations
    
    * added migration for new bridge param
    
    * updates
    
    ---------
    
    Co-authored-by: MarkCherepovskyi <mark.cherepovskyi@gmail.com>

--- database/schema/18-referrals.sql
+++ database/schema/18-referrals.sql
@@ -1,21 +1,29 @@
 -- +migrate Up
-CREATE TABLE referral {
-    id INT PRIMARY KEY,
+CREATE DOMAIN uint16 AS integer
+    CHECK (VALUE BETWEEN 0 AND 65535);
+
+
+CREATE TABLE bridge_referral
+(
+    id uint16 PRIMARY KEY,
     withdrawal_address TEXT,
     commission_rate INT
-};
+);
 
-CREATE TABLE referral_rewards {
-    referral_id UINT,
-    token_id BIGINT,
-    to_claim string,
-    total_claimed_amount string,
-};
+CREATE TABLE bridge_referral_rewards
+(
+    referral_id          uint16,
+    token_id             BIGINT,
+    to_claim             TEXT,
+    total_claimed_amount TEXT
+);
 
-ALTER TABLE bridge_transactions ADD COLUMN referral_id INT DEFAULT 0;
+ALTER TABLE bridge_transactions ADD COLUMN referral_id uint16 DEFAULT 0;
 
 -- +migrate Down
-DROP TABLE referral;
-DROP TABLE referral_rewards;
+DROP TABLE bridge_referral;
+DROP TABLE bridge_referral_rewards;
 
 ALTER TABLE bridge_transactions DROP COLUMN referral_id;
+
+DROP DOMAIN uint16;

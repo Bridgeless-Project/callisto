1457e73f746add83379fb3c999a57960955ee29d
EduardMikhrin <emihrin50@gmail.com>
2025-11-04 11:39:16 +0200

    added token_id

---
+++ database/schema/23-bridge-transaction-token-id.sql
@@ -0,0 +1,14 @@
+-- +migrate Up
+
+ALTER TABLE bridge_transactions ADD COLUMN token_id token_id INTEGER NOT NULL DEFAULT 0;
+
+UPDATE bridge_transactions AS bt
+SET token_id = bti.token_id
+    FROM bridge_tokens_info AS bti
+WHERE
+    bt.deposit_chain_id = bti.chain_id
+  AND lower(bt.deposit_token) = lower(bti.address);
+
+-- +migrate Down
+
+ALTER TABLE bridge_transactions DROP COLUMN token_id;

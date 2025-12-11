7bb8e843ef67c56f2aac31765b3ca887d17de8c5
EduardMikhrin <emihrin50@gmail.com>
2025-12-11 19:39:34 +0200

    fix migrations

--- database/schema/31-bridge-relayer-accounts.sql
+++ database/schema/31-bridge-relayer-accounts.sql
@@ -7,13 +7,17 @@ ALTER TABLE bridge_params
 ALTER COLUMN relayer_accounts TYPE TEXT[]
     USING ARRAY[relayer_accounts];
 
--- +migrate Down
+ALTER TABLE bridge_params
+    ALTER COLUMN relayer_accounts SET DEFAULT '{}';
 
 -- +migrate Down
 
 ALTER TABLE bridge_params
+    ALTER COLUMN relayer_accounts DROP DEFAULT;
+
+ALTER TABLE bridge_params
 ALTER COLUMN relayer_accounts TYPE VARCHAR(255)
-    USING relayer_accounts[1];
+        USING relayer_accounts[1];
 
 ALTER TABLE bridge_params
     RENAME COLUMN relayer_accounts TO relayer_account;

78ff74b1f8bfa297fe1ac7b6916371ee0c8056cb
EduardMikhrin <emihrin50@gmail.com>
2025-12-12 16:38:00 +0200

    update schemas

--- database/schema/32-bridge-transaction-withdrawal-hash.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-
-ALTER TABLE bridge_tranasctions ADD COLUMN withdrawal_tx_hash TEXT NOT NULL DEFAULT '';
-
--- +migrate Down
-
-ALTER TABLE bridge_transactions DROP COLUMN withdrawal_tx_hash;
